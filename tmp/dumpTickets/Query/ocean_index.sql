11cd77187b41c36aaae7819a4974721503252e4c
HerveH2C <contact@hcconsulting.fr>
2018-04-19 08:27:10 +0200

    Create ocean_index.sql

---
+++ tmp/dumpTickets/Query/ocean_index.sql
@@ -0,0 +1,6 @@
+-- --------------------------------------------------------
+-- 
+-- Indexation
+--
+USE `145852_gactechnology`;
+ALTER TABLE `tickets_appels` ADD `ta_id` INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`ta_id`);
